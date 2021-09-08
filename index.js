bb31670c5c24c1f0300406d26105f25ad941dcd3
Joshua Ellis <joshua.ellis.r@gmail.com>
2021-09-08 17:21:53 -0400

    Adds step 2

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-function renderMovies(movieArray) {
-  const movieHtmlArray = movieArray.map(function (currentMovie) {
-    return `<div class="movie col p-3">
-<div class="card bg-secondary text-light" style="width: 18rem;">
-  <img src="no_image.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${currentMovie.Title}</h5>
-    <p class="card-text">${currentMovie.Year}</p>
-    <a href="#" class="add-button btn btn-warning text" data-imdbid=${currentMovie.imdbID}>Go somewhere</a>
-  </div>
-</div>
-</div>`
-  })
-  return movieHtmlArray.join(''); //why do we need this?
-}
-
 // "Document Ready" block
 document.addEventListener('DOMContentLoaded', function () {
   // code here will execute after the document is loaded
@@ -23,22 +7,37 @@ document.addEventListener('DOMContentLoaded', function () {
     e.preventDefault();
     const moviesContainer = document.querySelector('.movies-container');
     moviesContainer.innerHTML = renderMovies(movieData);
+
+    moviesContainer.addEventListener('click', (e) => {
+      // console.log(e.target.tagName);
+      e.preventDefault();
+      if (e.target.classList.contains('add-button')) {
+        const movieID = e.target.dataset.imdbid; 
+        saveToWatchlist(movieID);
+      }
+    })
   })
 
-  document.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (e.target.classList.contains('add-button')) {
-      const movieID = e.target.dataset.imdbid; // make sure this works with "ID"
-      // console.log(movieID);
-    }
-  })
-
+  function renderMovies(movieArray) {
+    const movieHtmlArray = movieArray.map(function (currentMovie) {
+      return `<div class="movie col p-3">
+<div class="card bg-secondary text-light" style="width: 18rem;">
+  <img src=${currentMovie.Poster} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${currentMovie.Title}</h5>
+    <p class="card-text">${currentMovie.Year}</p>
+    <a href="#" class="add-button btn btn-warning text" data-imdbid=${currentMovie.imdbID}>Go somewhere</a>
+  </div>
+</div>
+</div>`
+    })
+    return movieHtmlArray.join(''); //Why do we need this? Because HTML can only read strings, and the above is JS. So you need the .join('') to convert to a string.
+  }
 
 });
 
 function saveToWatchlist(movieID) {
-  console.log(movieID) // I can't get this to work
-  const movie = movideData.find((currentMovie) => {
+  const movie = movieData.find((currentMovie) => {
     return currentMovie.imdbID == movieID;
   });
   let watchlistJSON = localStorage.getItem('watchlist');
